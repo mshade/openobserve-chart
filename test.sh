ac9584207cc937063684ce6d130d0e0645d1a5f9
Mike Shade <mshade@mshade.org>
2023-06-07 14:48:13 -0400

    :fire: cleanup gh-pages branch

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CT_IMAGE=quay.io/helmpack/chart-testing:v3.7.1
-
-docker run -it --network host --workdir=/data --volume ~/.kube/config:/root/.kube/config:ro --volume $PWD:/data $CT_IMAGE \
-    ct lint --charts openobserve
